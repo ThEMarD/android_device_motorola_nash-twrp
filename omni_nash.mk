b0620507a611944415f7e3844924805f67dd1a6d
Marc Bourgoin <themard85@gmail.com>
2020-03-22 15:26:46 +0430

    Revert "nash: Go back to prebuilt update_engine_sideload"
    This reverts commit 968717013507f70c13bd5957aece9be1a2b0a7e4.
    
    * source-built update_engine_sideload was fixed
    
    Change-Id: Ia8963d2faf7938c1769fcf8eae33528a7d50bb52

--- omni_nash.mk
+++ omni_nash.mk
@@ -37,6 +37,7 @@ PRODUCT_PACKAGES += \
     otapreopt_script \
     cppreopts.sh \
     update_engine \
+    update_engine_sideload \
     update_verifier
 
 # The following modules are included in debuggable builds only.
